e7cc4be8612e875c632405f6cfad25041457140c
yyogesh <yogeshggn1@gmail.com>
2022-06-15 13:25:37 +0530

    datatype

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine('b');
+
+Console.WriteLine(123);
+
+Console.WriteLine(12.3m);
+
+Console.WriteLine(true);
+
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
+
+string firstName = "Yogesh";
+string lastName = "Yadav";
+
+char userOption;
+
+int gameScore = 10;
+
+
+decimal partialPayment = 12.3m;
+
+bool processedCustomer;
+
+userOption = 'A';
+
+string firstName1;
+firstName1 = "Bob";
+
+
+string firstName2;
+// "Bob" = firstName1; // Wrong way to assign a value to a variable
+
+Console.WriteLine(firstName1);
+
+// Class means first letter always capitalized
+
+// Variable nama start with small letter and next word  start with capitalized
+
+Console.WriteLine("**************** Variable Reassign ******************");
+
+string firstName3;
+firstName3 = "Bob";
+Console.WriteLine(firstName3);
+firstName3 = "Beth";
+Console.WriteLine(firstName3);
+firstName3 = "Conrad";
+Console.WriteLine(firstName3);
+firstName3 = "Grant";
+Console.WriteLine(firstName3);
+
+Console.WriteLine("**************** Variable Reassign End ******************");
+
+string firstName4;
+//Console.WriteLine(firstName4); // Use of unassigned local variable 'firstName4' [dotnetconsolebasic]
+
+string city = "Delhi";
+
+city = "Mumbai";
+
+// city = 100; // Wrong way to assign a value to a variable
+
+var message = "Hello World";
+
+// message = 10.0m; // Cannot implicitly convert type 'decimal' to 'string'
+
+var message1 = 123;
+
+// string abc = 133;
+
+// var msg; // Implicitly-typed variables must be initialized before use
+
